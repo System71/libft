c224448891106e2baafcfa3e22034894d8275311
Pierre-Antoine Rigaudier <prigaudi@z1r11p4.42lyon.fr>
2025-03-26 15:18:42 +0100

    modif ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: prigaudi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 11:47:10 by prigaudi          #+#    #+#             */
-/*   Updated: 2024/11/20 18:17:44 by prigaudi         ###   ########.fr       */
+/*   Updated: 2025/03/26 15:18:27 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ int	ft_strncmp(const char *first, const char *second, size_t length)
 	unsigned char	mysecond;
 
 	i = 0;
+	myfirst = '\0';
+	mysecond = '\0';
 	while (i < length)
 	{
-		myfirst = (unsigned char)first[i];
-		mysecond = (unsigned char)second[i];
+		if (first)
+			myfirst = (unsigned char)first[i];
+		if (second)
+			mysecond = (unsigned char)second[i];
 		if (myfirst != mysecond || myfirst == '\0' || mysecond == '\0')
 			return (myfirst - mysecond);
 		i++;
